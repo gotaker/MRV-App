ad77df6b8e9f7dc1671c02e6c7dcc4f75af1f8cd
gotaker <gotaker12@gmail.com>
2025-08-18 21:18:27 -0400

    chore(eslint): extend Angular recommended flat configs (ts + template) and enable inline template linting
    - Use angular-eslint flat presets: tsRecommended and templateRecommended
    - Enable processInlineTemplates for @Component inline HTML
    - Keep selector conventions; remove manual template rule list

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,18 @@
 import 'jest-preset-angular/setup-jest';
 import type { Config } from 'jest';
-const config: Config = { preset: 'jest-preset-angular', testEnvironment: 'jsdom', setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'], testMatch: ['**/*.spec.ts'], moduleFileExtensions: ['ts','html','js','json'], transform: { '^.+\\.(ts|mjs|js|html)$': ['jest-preset-angular', { tsconfig: '<rootDir>/tsconfig.spec.json' }] } };
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  testMatch: ['**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      { tsconfig: '<rootDir>/tsconfig.spec.json' },
+    ],
+  },
+};
+
 export default config;
--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,1 +1,7 @@
-import '@testing-library/jest-dom';
+// New API in jest-preset-angular 14+
+// Prefer zoned environment for Angular tests
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+setupZoneTestEnv();
+
+// (If you hit a version constraint, temporary fallback works:)
+// import 'jest-preset-angular/setup-jest';
