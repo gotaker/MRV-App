ea13188311f53cd268df43cf30c5a92b57cb4556
gotaker <gotaker12@gmail.com>
2025-08-19 10:01:55 -0400

    test(jest): switch to CJS config and update Http testing; fix KPI spec to use provideHttpClientTesting

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'jest-preset-angular',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
-  testMatch: ['**/?(*.)+(spec).ts'],
-  moduleFileExtensions: ['ts', 'html', 'js'],
-};
