344c496ec3b69d01be7af6df35fd7a0a5eacfcdf
gotaker <gotaker12@gmail.com>
2025-08-28 01:08:51 -0400

    feat(ui): add top app bar with Login/Logout and nav links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +17,25 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   template: `
     <mat-toolbar color="primary">
-      <a routerLink="/" style="color:inherit;text-decoration:none;font-weight:600;">MRV</a>
+      <a routerLink="/" style="color:white;text-decoration:none;font-weight:600;">MRV</a>
       <span class="spacer"></span>
       <a mat-button routerLink="/dashboard">Dashboard</a>
-      <a mat-button routerLink="/login">Login</a>
+      <a mat-button routerLink="/kpis">KPIs</a>
+      <span class="spacer"></span>
+      <button *ngIf="!authed()" mat-button routerLink="/login">Login</button>
+      <button *ngIf="authed()" mat-button (click)="logout()">Logout</button>
     </mat-toolbar>
-
     <router-outlet />
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  private auth = inject(AuthService);
+
+  authed() {
+    return this.auth.isAuthed();
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+}
